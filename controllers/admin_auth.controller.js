924e44fd986f9f17eba1c7a596dcada557c8a028
SuvroMukherjee <subhra.m@onenesstechs.in>
2024-01-30 19:40:02 +0530

    push

--- controllers/admin_auth.controller.js
+++ controllers/admin_auth.controller.js
@@ -291,15 +291,11 @@ module.exports = {
 
       let agencyEmails = agencydata.map(e => e.corporate_email.toString());
 
-      let jodUID = jobPostingData?.job_id;
-
-      console.log(agencyEmails)
-
       sgMail.setApiKey(process.env.SENDGRID)
       const new_msg = {
         to: agencyEmails, // Change to your recipient
         from: 'info@hire2inspire.com', // Change to your verified sender
-        subject: `H2I | Calling All Talent Architects, A New Blueprint Awaits!`,
+        subject: "Calling All Talent Architects, A New Blueprint Awaits!",
         html: `
       <head>
           <title>Notification:New Job Posting</title>
@@ -308,7 +304,7 @@ module.exports = {
     <p>Greetings from hire2Inspire! We are thrilled to unveil a bold new blueprint that demands the expertise and finesse your agency can provide.</p>
     <p>Our latest mandate is not just another project – it's an opportunity to shape careers, transform organizations, and leave an indelible mark on the landscape of talent acquisition.</p>
     <p>Let us leverage our collective expertise to bring this blueprint to life.</p>
-    <p>Find your job<a href="https://hire2inspire.com/jobgrid" target="blank">${jodUID}</a>
+    <p>Job details and link of the job to be provided here posted on H2I, click here  <a href="https://hire2inspire.com/agency/job-details?id=${jobId}" target="blank">Find your job</p>
     <p>Regards,</p>
     <p>hire2Inspire</p>
     <p>&nbsp;</p>
