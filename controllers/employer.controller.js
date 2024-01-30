4872cdeff1d7f2049c6ea226f40e022dd1b8b878
SuvroMukherjee <subhra.m@onenesstechs.in>
2024-01-30 16:07:14 +0530

    push

--- controllers/employer.controller.js
+++ controllers/employer.controller.js
@@ -216,7 +216,7 @@ module.exports = {
 
       sgMail.setApiKey(process.env.SENDGRID)
       const new_msg = {
-        to: "subhramukherjee560@gmail.com", // Change to your recipient
+        to: "hire2inspireh2i@gmail.com", // Change to your recipient
         from: 'info@hire2inspire.com', // Change to your verified sender
         subject: `New Employeer Registration`,
         html: `
